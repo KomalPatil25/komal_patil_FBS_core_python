33e7e7ca7cd10f2d4c2bf6b4deba64a6506e43cd
Komal Patil <kp3807049@gmail.com>
2025-08-18 09:00:08 +0530

    Delete TEST_01.PY directory

--- TEST_01.PY/simpleinterest_02.py
+++
@@ -1,7 +0,0 @@
-Principal = int(input("Enter the principal: "))
-Rate = int(input("enter the Rate: "))
-Time = int(input("enter the Time: "))
-
-simple_interest = (Principal * Rate * Time) / 100
-
-print(f"The simple interest is {simple_interest}")
