bc1595333db8347a667f9db7a272a49a3aaa92b2
Komal Patil <kp3807049@gmail.com>
2025-08-18 09:04:55 +0530

    Delete Test_02.py directory

--- Test_02.py/totalCostPainting_04.py
+++
@@ -1,9 +0,0 @@
-height = int(input("enter the height: "))
-width = int(input("enter the width: "))
-cost_sq_meter = int(input("enter cost of painting per square meter: "))
-
-area_one_wall = height * width
-total_area = area_one_wall * 4
-total_cost = total_area *cost_sq_meter
-
-print(f'Total area to be painted is {total_area}.\nTotal cost of painting {total_cost}')
