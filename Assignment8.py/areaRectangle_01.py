8498b336bd951d6f2d12f348ffc549371faa569e
Komal Patil <kp3807049@gmail.com>
2025-08-18 09:03:52 +0530

    Delete Assignment8.py directory

--- Assignment8.py/areaRectangle_01.py
+++
@@ -1,9 +0,0 @@
-def areaRec(length, breadth):
-
-    area = length * breadth
-    return area
-
-length = int(input("Enter the length: "))
-breadth = int(input("Enter the breadth: "))
-res = areaRec(length, breadth)
-print(f"The area of rectangle is {res}.")
--- Assignment8.py/q3C.py
+++
@@ -1,9 +0,0 @@
-def powerSum(num):
-    total = 0
-    for i in range(1, num + 1):
-        total = total + (i ** i)
-    return total
-
-num = int(input("Enter the number: "))
-result = powerSum(num)
-print(f"The sum is: {result}")
